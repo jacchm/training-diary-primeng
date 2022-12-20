27f39efd5237b88e880b01d7fff59e41cb796ebe
jacchm <jacekchmiel03@gmail.com>
2022-12-20 02:59:19 +0100

    build menu basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,70 @@
-import { SidebarModule } from 'primeng/sidebar';
-import { SidebarService } from './sidebar.service';
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
+import {fromEvent, Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  display = false;
-  width = 500;
-  
-  @ViewChild('sidenav', {static: false}) 
-  sidenav!: ElementRef;
+export class AppComponent implements OnInit, OnDestroy {
+  display = true;
+  items: MenuItem[] = [];
+  mobileView: boolean = false;
+  resizeObservable$: Observable<Event> = new Observable<Event>();
+  resizeSubscription$: Subscription = new Subscription();
 
-  constructor(private sidebarService: SidebarService) {
+  dockItems: MenuItem[] = [];
+  extended: boolean = true;
+
+  ngOnInit() {
+    this.resizeObservable$ = fromEvent(window, 'resize');
+    this.resizeSubscription$ = this.resizeObservable$.subscribe(() => {
+      this.display = !(window.innerWidth <= 900);
+    });
+    this.setupMenuItems();
   }
 
-  ngAfterViewInit(): void {
-    console.log('DOM property value 1: ' + this.sidenav.nativeElement);
+  ngOnDestroy() {
+    this.resizeSubscription$.unsubscribe();
   }
 
-  getDOMPropertyValue(): any {
-    this.display = true;
+  private setupMenuItems() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: "assets/images/dock/house-solid.svg"
+      },
+      {
+        label: 'Calendar',
+        icon: "assets/images/dock/calendar-days-solid.svg"
+      },
+      {
+        label: 'Trainings',
+        icon: "assets/images/dock/dumbbell-solid.svg"
+      },
+      {
+        label: 'Progress',
+        icon: "assets/images/dock/chart-line-solid.svg"
+      },
+      {
+        label: 'Comments',
+        icon: "assets/images/dock/comments-solid.svg"
+      },
+      {
+        label: 'My Profile',
+        icon: "assets/images/dock/user-solid.svg"
+      },
+      {
+        label: 'Trainee',
+        icon: "assets/images/dock/users-solid.svg"
+      },
+      {
+        label: 'Settings',
+        icon: "assets/images/dock/gear-solid.svg"
+      }
+    ];
   }
 
-  print(): any {
-    console.log('DOM property value 2: ' + this.sidenav.nativeElement);
-  }
-
-  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {SidebarModule} from 'primeng/sidebar';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {ButtonModule} from 'primeng/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
-import {MenuItem} from 'primeng/api';                  //api
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AccordionModule} from 'primeng/accordion'; //accordion and accordion tab
+import {TrainingComponent} from './training/training.component';
+import {MenuModule} from "primeng/menu";
+import {SidebarModule} from "primeng/sidebar";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SidebarModule,
     ButtonModule,
-    AccordionModule
+    AccordionModule,
+    SidebarModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
